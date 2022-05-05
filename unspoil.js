4db8ac13c6f21bf7d83634bf22f56f6ab47851e2
Sam Scheffler <sam.scheffler@westacademic.com>
2022-05-05 10:17:26 -0500

    initial commit

---
+++ unspoil.js
@@ -0,0 +1,9 @@
+
+
+// document.getElementsByClassName('ytp-progress-bar-container')[0].style.visibility = 'hidden';
+// document.getElementsByClassName('ytp-time-separator')[0].style.visibility = 'hidden';
+// document.getElementsByClassName('ytp-time-duration')[0].style.visibility = 'hidden';
+// var thumbnailTimes = document.getElementsByTagName('ytd-thumbnail-overlay-time-status-renderer')
+// for(let i = 0; i < thumbnailTimes.length; i++){
+//     thumbnailTimes[i].style.visibility = 'hidden'
+// }
